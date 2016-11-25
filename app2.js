d9b52a7548e27740a671d421830575ea418bb5fe
bvellek <bvellek@uw.edu>
2016-11-25 10:22:59 -0800

    fixed delete, empty item, and check bugs

--- app2.js
+++ app2.js
@@ -1,153 +1,139 @@
-//completedItem and toDeleteItem functions - how do I make `this` work
-//does my findItemIndex function work?
-//couldnt figure out how to give my labels and inputs proper `id` & `for`
-
-
-
-
 'use strict';
 
 // State Object
 var state = {
   items: [],
+  indexNum: 0
 };
 
 
 // Modify functions
-function addItem(state, item) {
+function addItem(state, item, indexVal) {
   state.items.push({
+    index: indexVal,
     name: item,
     completed: false,
     toDelete: false
   });
-};
+}
 
-function completedItem(state, itemIndex, that) {
-  console.log(state.items, itemIndex);
+function completedItem(state, checkIndex, that) {
   if ($(that).is(':checked')) {
-    state.items[itemIndex].completed = true;
+    state.items[checkIndex].completed = true;
+    $(that).closest('h3').addClass('shopping-item__checked');
   } else {
-    state.items[itemIndex].completed = false;
+    state.items[checkIndex].completed = false;
+    $(that).closest('h3').removeClass('shopping-item__checked');
   }
-};
+}
 
-function toDeleteItem(state, itemIndex, that) {
-  console.log(state, itemIndex, that);
-  console.log($(that).is(':checked'));
+function toDeleteItem(state, deleteIndex, that) {
   if ($(that).is(':checked')) {
-
-    state.items[itemIndex].toDelete = true;
+    state.items[deleteIndex].toDelete = true;
+    $(that).closest('li').addClass('delete-check');
   } else {
-    state.items[itemIndex].toDelete = false;
+    state.items[deleteIndex].toDelete = false;
+    $(that).closest('li').removeClass('delete-check');
   }
-};
-
-function deleteItems(state) {
-  for (var i=0; i < state.length; i++) {
-      if (state.items[i].toDelete) {
-        state.items.splice(i, 1);
-      }
-  }
-};
-
-function findItemIndex(state, key, value) {
-  console.log(state, key, value);
-  for (var i=0; i < state.items.length; i++) {
-    console.log(state.items[i][key]);
-    if (state.items[i][key] === value) {
-      return i;
-    }
-  } return -1;
 }
 
 
 // Render functions
 
-var renderItem = function(element, item) {
-    var itemTemplate = '<li><h3 class="shopping-item"><input type="checkbox" id="" name=""><label  for="">';
-    itemTemplate += item;
-    itemTemplate += '</label></h3><div class="shopping-item-controls"><label for="">Delete<span class="visually-hidden">';
-    itemTemplate += item;
-    itemTemplate += '</span></label><input type="checkbox" id="" name=""></div></li>';
-    element.append(itemTemplate);
+var itemTemplate = $(
+
+'<li class="list-item">' +
+  '<h3 class="shopping-item">' +
+    '<input type="checkbox" id="" name="">' +
+    '<label for=""></label>' +
+  '</h3>' +
+
+  '<div class="shopping-item-controls">' +
+    '<label for="">Delete<span class="visually-hidden"></span></label>' +
+    '<input type="checkbox" id="" name="">' +
+  '</div>' +
+'</li>'
+);
+
+
+function createItem(item) {
+  var newItem = $(itemTemplate).clone();
+  var itemIndex = item.index;
+  var itemName = item.name;
+  var itemCheck = item.completed
+
+  if (itemCheck) {
+    newItem.find('.shopping-item').addClass('shopping-item__checked');
+    newItem.find('h3 input').attr("checked", true);
   };
+  newItem.find('h3 label').attr('for', itemIndex);
+  newItem.find('h3 input').attr('name', itemName + itemIndex);
+  newItem.find('h3 input').attr('id', itemIndex);
+  newItem.find('h3 label').text(itemName);
+  newItem.find('.shopping-item-controls label').attr('for', ('delete' + itemIndex));
+  newItem.find('.shopping-item-controls input').attr('id', ('delete' + itemIndex));
+  newItem.find('.shopping-item-controls input').attr('name', ('delete' + itemName));
+  newItem.find('.shopping-item-controls span').text(itemName);
+  if (item.complete) {
+    newItem.find($('h3').addClass('shopping-item__checked'));
+    newItem.find($('h3 input').prop('checked'));
+  };
+  return newItem[0].outerHTML;
+}
 
 
-var renderList = function(state, element) {
-  console.log('renderList');
-  console.log(state);
-  if (!state.items.length) {
-    element.empty();
-  }
+function displayItems(state) {
+
   var htmls = '';
-  for (var i=0; i < state.items.length; i++) {
-    console.log(state.items[i].name);
-    var nameValue = state.items[i].name;
-    var completedValue = state.items[i].completed;
-    var completeClass = '';
-    var completeCheck = '';
-    var itemMaker = function (nameValue, completedValue) {
-      if (completedValue) {
-        completeClass = 'shopping-item__checked';
-        completeCheck = 'checked';
-      } else {
-        completeClass = "";
-        completeCheck = "";
-      };
-      var itemTemplate = '<li><h3 class="shopping-item"';
-      itemTemplate += completeClass;
-      itemTemplate += '><input type="checkbox" id="" name=""';
-      itemTemplate += completeCheck;
-      itemTemplate += '><label  for=""';
-      itemTemplate += '>';
-      itemTemplate += nameValue;
-      itemTemplate += '</label></h3><div class="shopping-item-controls"><label for="">Delete<span class="visually-hidden">';
-      itemTemplate += nameValue;
-      itemTemplate += '</span></label><input type="checkbox" id="" name=""></div></li>';
-      return itemTemplate;
-      };
-    htmls += itemMaker(nameValue, completedValue);
-  };
-  element.html(htmls);
-};
+  state.items.forEach(function(item){
+    htmls += createItem(item);
+  });
+
+  $('.shopping-list').html(htmls);
+  }
+
+
 
 
 // Event Listeners
 
-//Form Submit
-$('#js-shopping-list-form').submit(function(event) {
-  event.preventDefault();
-  addItem(state, $('#js-shopping-list-form input').val());
-  renderItem($('.shopping-list'), $('#js-shopping-list-form input').val());
-  $('#js-shopping-list-form input').val('');
-});
+$(document).ready(function() {
 
-
-//Checkbox Completed
-$('.shopping-list').on('change', 'li h3 input', function(event) {
-    $(this).closest('h3').toggleClass('shopping-item__checked');
-    console.log(name);
-    var itemIndex = findItemIndex(state, 'name', $(this).siblings('label').text());
-    completedItem(state, itemIndex, $(this));
-});
-
-
-//Checkbox Deleted
-$('.shopping-list').on('change', 'li .shopping-item-controls input', function(event) {
-    $(this).closest('li').toggleClass('delete-check');
-    var itemIndex = findItemIndex(state, 'name', $(this).siblings('label').children('span').text());
-    toDeleteItem(state, itemIndex, $(this));
-});
-
-//Confirm Deletes
-
-$('.confirm-delete').click(function (event) {
-  event.preventDefault();
-  console.log(state.items);
-  state.items = state.items.filter(function (value, index) {
-    console.log(value, index);
-    return !value.toDelete;
+  //Form Submit
+  $('#js-shopping-list-form').submit(function(e) {
+    e.preventDefault();
+    state.indexNum ++;
+    addItem(state, $('#js-shopping-list-form input').val(), state.indexNum);
+    displayItems(state);
+    $('#js-shopping-list-form input').val('');
   });
-  console.log(state.items);
-  renderList(state, $('.shopping-list'));
+
+
+  //Checkbox Completed
+  $('.shopping-list').on('change', 'li h3 input', function(e) {
+      var checkIndex = $(this).attr('id') - 1;
+      completedItem(state, checkIndex, $(this));
+  });
+
+
+  //Checkbox Deleted
+  $('.shopping-list').on('change', 'li .shopping-item-controls input', function(e) {
+    var deleteIndex = $(this).closest('div').siblings('h3').children('input').attr('id') - 1;
+    toDeleteItem(state, deleteIndex, $(this));
+  });
+
+  //Confirm Deletes
+
+  $('.confirm-delete').click(function (e) {
+    e.preventDefault();
+    console.log(state.items);
+    state.items = state.items.filter(function (value, index) {
+      console.log(value, index);
+      return !value.toDelete;
+    });
+    console.log(state.items);
+    displayItems(state);
+  });
+
+
 });
